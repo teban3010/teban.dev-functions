656a205e78bd2b08773cc5e676d031993438e882
Teban <eyanez.89@gmail.com>
2020-08-21 13:20:40 -0300

    Initial commit

--- puppeteer-proxy/index.js
+++ puppeteer-proxy/index.js
@@ -1,14 +1,20 @@
 const linkPreviewGenerator = require("link-preview-generator");
 
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+  context.log("JavaScript HTTP trigger function processed a request.");
+  try {
+    const url = req.query.url || (req.body && req.body.url);
 
-    const url = (req.query.url || (req.body && req.body.url));
-    
     const previewData = await linkPreviewGenerator(url);
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: previewData
+      // status: 200, /* Defaults to 200 */
+      body: previewData,
     };
-}
+  } catch (error) {
+    context.res = {
+      status: 400 /* Defaults to 200 */,
+      body: JSON.stringify( error),
+    };
+  }
+};
